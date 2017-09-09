d0442e883552ce2009c960ff4a2aeaba5b29291c
Galen Palmer <palmergs@gmail.com>
2017-09-09 18:29:49 -0400

    python middleware can create protobuf messages

--- django/bmore_middleware.py
+++ django/bmore_middleware.py
@@ -2,6 +2,8 @@ import socket
 import select
 import os
 import sys
+from chat.bmore.messages_pb2 import HttpRequest, KeyValue
+from cgi import parse_qs, escape
 
 class BmoreMiddleware(object):
     def __init__(self, app):
@@ -11,12 +13,33 @@ class BmoreMiddleware(object):
 
     def __call__(self, environ, start_response):
         print("In BmoreMiddleware call...")
+        http_request = self.build_http_request(environ)
+        print(http_request)
         return self.app(environ, start_response)
+
+    def build_http_request(self, environ):
+        print(environ)
+        req = HttpRequest()
+        req.request_method = environ.get('REQUEST_METHOD', 'GET')
+        req.host = environ.get('SERVER_NAME', '')
+        req.port = int(environ.get('SERVER_PORT', '0'))
+        req.script = environ.get('SCRIPT_NAME', '')
+        req.path = environ.get('PATH_INFO', '')
+        req.ip = environ.get('REMOTE_ADDR', '')
+        
+        d = parse_qs(environ['QUERY_STRING'])
+        for key in d:
+            req.parameters[key].key = key
+            for v in d[key]:
+                req.parameters[key].value.append(v)
+
+        return req
 
     def generate_socket(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect(('localhost', 35678))
         sock.setblocking(True)
         return sock
+
 
 
