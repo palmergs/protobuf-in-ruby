9be3b36c90bc501973c63cf348af8bcfbd46d5ae
Galen Palmer <palmergs@gmail.com>
2017-09-09 15:54:22 -0400

    start on django client

--- django/bmore/wsgi.py
+++ django/bmore/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
-from chat import BmoreMiddleware
+from bmore_middleware import BmoreMiddleware
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bmore.settings")
--- django/bmore_middleware.py
+++ django/bmore_middleware.py
@@ -1,3 +1,8 @@
+import socket
+import select
+import os
+import sys
+
 class BmoreMiddleware(object):
     def __init__(self, app):
         print("In BmoreMiddleware init...")
@@ -7,5 +12,11 @@ class BmoreMiddleware(object):
     def __call__(self, environ, start_response):
         print("In BmoreMiddleware call...")
         return self.app(environ, start_response)
+
+    def generate_socket(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(('localhost', 35678))
+        sock.setblocking(True)
+        return sock
 
 
