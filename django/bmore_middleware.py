dbc084fddc487d1bef3b38c3270d3a201e652d86
Galen Palmer <palmergs@gmail.com>
2017-09-10 09:00:59 -0400

    sending messages but go service can't parse

--- django/bmore_middleware.py
+++ django/bmore_middleware.py
@@ -1,6 +1,7 @@
 import socket
 import select
 import os
+import io
 import sys
 from chat.bmore.messages_pb2 import HttpRequest, KeyValue, Firewall
 from cgi import parse_qs, escape
@@ -23,14 +24,13 @@ class BmoreMiddleware(object):
             return self.app(environ, start_response)
 
     def build_http_request(self, environ):
-        print(environ)
         req = HttpRequest()
-        req.request_method = environ.get('REQUEST_METHOD', 'GET')
-        req.host = environ.get('SERVER_NAME', '')
-        req.port = int(environ.get('SERVER_PORT', '0'))
-        req.script = environ.get('SCRIPT_NAME', '')
-        req.path = environ.get('PATH_INFO', '')
-        req.ip = environ.get('REMOTE_ADDR', '')
+        req.request_method = environ.get('REQUEST_METHOD', "GET")
+        req.host = environ.get('SERVER_NAME', "")
+        req.port = int(environ.get('SERVER_PORT', "0"))
+        req.script = environ.get('SCRIPT_NAME', "")
+        req.path = environ.get("PATH_INFO", "")
+        req.ip = environ.get('REMOTE_ADDR', "")
         
         d = parse_qs(environ['QUERY_STRING'])
         for key in d:
@@ -41,11 +41,28 @@ class BmoreMiddleware(object):
         return req
 
     def send_message(self, message):
+        print(message)
         packed = message.SerializeToString()
         print(packed)
-        firewall = Firewall()
-        firewall.block_it = True
-        return firewall
+        try:
+            sock = self.generate_socket()
+            sock.send(packed)
+            received = self.receive_data(sock)
+
+            firewall = Firewall()
+            firewall.ParseFromString(received)
+            print(firewall)
+            return firewall
+        finally:
+            if sock is not None:
+                sock.close()
+        
+    def receive_data(self, sock):
+        print("In receive data...")
+        buf = bytearray()
+        byte_count = sock.recv_into(buf)
+        print(byte_count)
+        return buf
 
     def generate_socket(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
